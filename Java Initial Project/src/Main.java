012f7a182e9531b0a048da5005a47f701f5b3ec5
dawpod <podrazad@gmail.com>
2023-12-09 13:34:23 +0100

    Firt commit

---
+++ Java Initial Project/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String name = "Dawid";
+        int liczba = 10;
+
+        System.out.println(name);
+        System.out.println(liczba);
+    }
+}

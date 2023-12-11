3b6a52610076d30b429394aff44317b039104bf1
dawpod <podrazad@gmail.com>
2023-12-11 21:05:32 +0100

    Java Class and Objects training

--- Java Initial Project/src/Main.java
+++ Java Initial Project/src/Main.java
@@ -1,11 +1,28 @@
-public class Main {
+public class Company {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee1 = new Employee();
+        employee1.imie = "Mariusz";
+        employee1.nazwisko = "Czarny";
+        employee1.rok = 1989;
+        employee1.staz = 2.5;
 
-        String name = "Dawid";
-        int liczba = 10;
+        Employee employee2 = new Employee();
+        employee2.imie = "Jan";
+        employee2.nazwisko = "Kowalski";
+        employee2.rok = 1986;
+        employee2.staz = 3.1;
 
-        System.out.println(name);
-        System.out.println(liczba);
+        Employee employee3 = new Employee();
+        employee3.imie = "Kamil";
+        employee3.nazwisko = "Bury";
+        employee3.rok = 1962;
+        employee3.staz = 7.3;
+
+
+        System.out.println("Pierwszy pracownik: "+employee1.imie+" "+employee1.nazwisko);
+        System.out.println("Drugi pracownik: "+employee2.imie+" "+employee2.nazwisko);
+        System.out.println("Trzeci pracownik: "+employee3.imie+" "+employee3.nazwisko);
     }
 }
+
+
